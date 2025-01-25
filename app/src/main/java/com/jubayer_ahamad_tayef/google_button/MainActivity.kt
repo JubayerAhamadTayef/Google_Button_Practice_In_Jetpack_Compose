6a00b25e4cc1e0c3484b377b4b48eba05236fd4f
JubayerAhamadTayef <jubayerahmedtayef.579462@gmail.com>
2025-01-25 10:03:58 +0600

    Create a new empty jetpack compose android project!

---
+++ app/src/main/java/com/jubayer_ahamad_tayef/google_button/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.jubayer_ahamad_tayef.google_button
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.jubayer_ahamad_tayef.google_button.ui.theme.Google_ButtonTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Google_ButtonTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Google_ButtonTheme {
+        Greeting("Android")
+    }
+}
